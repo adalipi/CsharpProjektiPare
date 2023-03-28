028d7a0623a5fc7f65fc832bf925aab8e90b107b
a.dalipi@live.com <a.dalipi@live.com>
2023-03-28 11:17:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using ProjektiPare;
+using Quartz.Logging;
+
+
+IConfiguration configFile = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true).Build();
+
+var services = new ServiceCollection();
+services
+    .AddLogging(log => { log.ClearProviders(); log.AddNLog(); })
+    .AddSingleton(configFile)
+    .AddScoped<TestLogim>()
+    ;
+
+using (var serviceProvider = services.BuildServiceProvider())
+{
+    var objekti = serviceProvider.GetRequiredService<TestLogim>();
+    objekti.Shenim();
+}
+
---
+++ TestLogim.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ProjektiPare
+{
+    public class TestLogim
+    {
+        private readonly ILogger<TestLogim> _logger;
+        private readonly IConfiguration _config;
+
+        public TestLogim(ILogger<TestLogim> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public void Shenim() 
+        {
+            var test = _config["Folderi"];
+            _logger.LogInformation($"shenuam dicka ne log: {test}");
+            _logger.LogError("gabim gabim..");
+        }
+    }
+}
